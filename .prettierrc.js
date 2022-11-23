443bd9b5431eca185566a7885ce0efc69886d183
Jeong-IK <jjy20315@gmail.com>
2022-11-23 22:06:53 +0900

    prettierrc.js 생성 및 설정 추가

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true,
+  tabWidth: 4,
+  printWidth: 80,
+  arrowParens: 'avoid',
+  endOfLine: "auto",
+};
---
+++ pages/store.ts
@@ -0,0 +1,11 @@
+import create from "zustand";
+
+// interface Logindata {
+//     email: string;
+//     password: string;
+//     loginaction: () => void;
+// }
+
+export const Logintest = create(data => {
+    console.log(data);
+});
