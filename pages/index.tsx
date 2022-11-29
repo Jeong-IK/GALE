057161461a53f3167fec85c90e69d166dd5cdaeb
Jeong-IK <jjy20315@gmail.com>
2022-11-30 01:34:41 +0900

    form 비제어 컴포넌트 변경 <Error: CORS Error >

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,40 @@
 import type { NextPage } from "next";
 import { useRef } from "react";
-import { useMemberStore } from "./store";
+import axios from "axios";
 
 const Home: NextPage = () => {
-    const Getdata = useMemberStore(state => {
-        return state;
-    });
     const Inputemail = useRef<HTMLInputElement>(null);
     const Inputpasswd = useRef<HTMLInputElement>(null);
+    const Loginaction = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (Inputemail.current?.value && Inputpasswd.current?.value) {
+            axios
+                .post(
+                    "http://175.212.160.106:7777/user/login",
+                    {
+                        Email: Inputemail.current.value,
+                        Password: Inputpasswd.current.value,
+                    },
+                    { withCredentials: true }
+                )
+                .then(response => {
+                    alert(response.status);
+                });
+        } else {
+            Inputemail.current?.focus();
+        }
+    };
 
     return (
-        <>
-            <h1>{Getdata.email}</h1>
-            <h1>{Getdata.password}</h1>
-            <form>
-                <input
-                    type="text"
-                    placeholder="아이디를 입력해주세요"
-                    ref={Inputemail}
-                />
-                <input type="password" id="passwd" ref={Inputpasswd} />
-                <button type="submit">데이터 삽입</button>
-            </form>
-        </>
+        <form onSubmit={Loginaction}>
+            <input
+                type="text"
+                placeholder="아이디를 입력해주세요"
+                ref={Inputemail}
+            />
+            <input type="password" ref={Inputpasswd} />
+            <button type="submit">데이터 삽입</button>
+        </form>
     );
 };
 
