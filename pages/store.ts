b39b98ce93da029c0603342102c9d1fd443534e6
IwannaBerealnerD <iwannaberealnerd@gmail.com>
2022-12-16 18:48:21 +0900

    [setting] eslint arrow body 설정 변경

--- pages/store.ts
+++ pages/store.ts
@@ -11,42 +11,18 @@ import {
 
 // Modal 활성화 및 Modaltype
 
-export const useModal = create<Modalstatus>()(set => {
-    return {
+export const useModal = create<Modalstatus>()(set => ({
         modalOption: 0,
-        setModaloption: (req: Modaloption) => {
-            return set(() => {
-                return { modalOption: req };
-            });
-        },
-    };
-});
+        setModaloption: (req: Modaloption) => set(() => ({ modalOption: req })),
+    }));
 
-export const useInputError = create<SignupErrorMsgType>()(set => {
-    return {
+export const useInputError = create<SignupErrorMsgType>()(set => ({
         emailErrorMsg: null,
         pwdErrorMsg: null,
         cfmPwdErrorMsg: null,
         nickNameErrorMsg: null,
-        setEmailErrorMsg: (errorMsg: EmailErrorMsgType) => {
-            return set(() => {
-                return { emailErrorMsg: errorMsg };
-            });
-        },
-        setPwdErrorMsg: (errorMsg: PwdErrorMsgType) => {
-            return set(() => {
-                return { pwdErrorMsg: errorMsg };
-            });
-        },
-        setCfmPwdErrorMsg: (errorMsg: CfmPwdErrorMsgType) => {
-            return set(() => {
-                return { cfmPwdErrorMsg: errorMsg };
-            });
-        },
-        setNickNameErrorMsg: (errorMsg: NickNameErrorMsgType) => {
-            return set(() => {
-                return { nickNameErrorMsg: errorMsg };
-            });
-        },
-    };
-});
+        setEmailErrorMsg: (errorMsg: EmailErrorMsgType) => set(() => ({ emailErrorMsg: errorMsg })),
+        setPwdErrorMsg: (errorMsg: PwdErrorMsgType) => set(() => ({ pwdErrorMsg: errorMsg })),
+        setCfmPwdErrorMsg: (errorMsg: CfmPwdErrorMsgType) => set(() => ({ cfmPwdErrorMsg: errorMsg })),
+        setNickNameErrorMsg: (errorMsg: NickNameErrorMsgType) => set(() => ({ nickNameErrorMsg: errorMsg })),
+    }));
